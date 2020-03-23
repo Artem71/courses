d187f4e572ab99a5a84d84e0bd8ae5343c788b4f
Artem71 <fich71@mail.ru>
2020-03-23 22:50:16 +0300

    mongo-session

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const session = require('express-session')
+const MongoStore = require('connect-mongodb-session')(session)
 const homeRoutes = require('./routes/home')
 const addRoutes = require('./routes/add')
 const ordersRoutes = require('./routes/orders')
@@ -12,33 +13,29 @@ const authRoutes = require('./routes/auth')
 const User = require('./models/user')
 const varMiddleware = require('./middleware/variables')
 
+const MONGODB_URI = 'mongodb+srv://artem:z1l6oOkTLjopkSSH@cluster0-sahlq.mongodb.net/shop'
 const app = express()
 // Настраиваем шаблонизатор
 const hbs = exphbs.create({
     defaultLayout: 'main',
     extname: 'hbs'
 })
+const store = new MongoStore({
+    collection: 'sessions',
+    uri: MONGODB_URI
+})
 
 app.engine('hbs', hbs.engine) // Регистрируем что такой движок есть
 app.set('view engine', 'hbs') // Начинаем его использовать
 app.set('views', 'views') // Указываем где будут хранится все наши шаблоны (По умолчанию стоит views)
-
-app.use(async (req, res, next) => {
-    try {
-        const user = await User.findById('5e7125da1afdaf0e749678b4')
-        req.user = user
-        next()
-    } catch (e) {
-        console.log(e)
-    }
-})
 
 app.use(express.static(path.join(__dirname, 'public'))) // Указываем экспресу папку статических файлов
 app.use(express.urlencoded({extended: true})) // Для получения данных с форм
 app.use(session({
     secret: 'some secret value',
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    store
 }))
 app.use(varMiddleware)
 
@@ -54,20 +51,10 @@ const port = process.env.port || 3000
 
 async function start() {
     try {
-        const url = 'mongodb+srv://artem:z1l6oOkTLjopkSSH@cluster0-sahlq.mongodb.net/shop'
-        await mongoose.connect(url, {
+        await mongoose.connect(MONGODB_URI, {
             useNewUrlParser: true, 
             useUnifiedTopology:true
         })
-        const candidate = await  User.findOne()
-        if (!candidate) {
-            const user = new User({
-                email: 'artem@mail.ru',
-                name: 'Artem',
-                cart: {items: []}
-            })
-            await user.save()
-        } 
         app.listen(port, () => {
             console.log(`Server has been started on port ${port}`)
         })
--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const {Router} = require('express')
+const User = require('../models/user')
 const router = new Router()
 
 router.get('/login', async (req, res) => {
@@ -15,8 +16,16 @@ router.get('/logout', async (req, res) => {
 })
 
 router.post('/login', async (req, res) => {
+    const user = await User.findById('5e7125da1afdaf0e749678b4')
+    req.session.user = user
     req.session.isAuthenticated = true
-    res.redirect('/')
+    req.session.save(err => {
+        if (err) {
+            throw err
+        }
+        res.redirect('/')
+    })
+    
 })
 
 module.exports = router
